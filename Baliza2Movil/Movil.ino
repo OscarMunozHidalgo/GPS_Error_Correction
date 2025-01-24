287042d7b819ace26273140dc57de58e0058605d
PabloCrb <146728217+PabloCrb@users.noreply.github.com>
2025-01-24 15:59:05 +0000

    Movil

--- Baliza2Movil/Movil.ino
+++ Baliza2Movil/Movil.ino
@@ -1,1 +1,185 @@
+#include <SPI.h>             
+#include <LoRa.h>
+#include <Arduino_PMIC.h>
 
+#define TX_LAPSE_MS          1000
+
+// NOTA: Ajustar estas variables 
+const uint8_t localAddress = 0x32;     // Dirección de este dispositivo
+uint8_t destination = 0x33;            // Dirección de destino, 0xFF es la dirección de broadcast
+
+volatile bool txDoneFlag = true;       // Flag para indicar cuando ha finalizado una transmisión
+volatile bool transmitting = false;
+
+// Estructura para almacenar la configuración de la radio
+typedef struct {
+  uint8_t bandwidth_index;
+  uint8_t spreadingFactor;
+  uint8_t codingRate;
+  uint8_t txPower; 
+} LoRaConfig_t;
+
+double bandwidth_kHz[10] = {7.8E3, 10.4E3, 15.6E3, 20.8E3, 31.25E3,
+                            41.7E3, 62.5E3, 125E3, 250E3, 500E3 };
+
+LoRaConfig_t thisNodeConf   = { 8, 7, 5, 2};
+
+// --------------------------------------------------------------------
+// Setup function
+// --------------------------------------------------------------------
+void setup() 
+{
+  Serial.begin(115200);  
+  while (!Serial); 
+
+  Serial.println("LoRa Duplex with TxDone and Receive callbacks");
+  Serial.println("Using binary packets");
+  
+  // Es posible indicar los pines para CS, reset e IRQ pins (opcional)
+  // LoRa.setPins(csPin, resetPin, irqPin);// set CS, reset, IRQ pin
+
+  
+  if (!init_PMIC()) {
+    Serial.println("Initilization of BQ24195L failed!");
+  }
+  else {
+    Serial.println("Initilization of BQ24195L succeeded!");
+  }
+
+  if (!LoRa.begin(868E6)) {      // Initicializa LoRa a 868 MHz
+    Serial.println("LoRa init failed. Check your connections.");
+    while (true);                
+  }
+
+  // Configuramos algunos parámetros de la radio
+  LoRa.setSignalBandwidth(long(bandwidth_kHz[thisNodeConf.bandwidth_index])); 
+                                  // 7.8E3, 10.4E3, 15.6E3, 20.8E3, 31.25E3
+                                  // 41.7E3, 62.5E3, 125E3, 250E3, 500E3 
+                                  // Multiplicar por dos el ancho de banda
+                                  // supone dividir a la mitad el tiempo de Tx
+                                  
+  LoRa.setSpreadingFactor(thisNodeConf.spreadingFactor);     
+                                  // [6, 12] Aumentar el spreading factor incrementa 
+                                  // de forma significativa el tiempo de Tx
+                                  // SPF = 6 es un valor especial
+                                  // Ver tabla 12 del manual del SEMTECH SX1276
+  
+  LoRa.setCodingRate4(thisNodeConf.codingRate);         
+                                  // [5, 8] 5 da un tiempo de Tx menor
+                                  
+  LoRa.setTxPower(thisNodeConf.txPower, PA_OUTPUT_PA_BOOST_PIN); 
+                                  // Rango [2, 20] en dBm
+                                  // Importante seleccionar un valor bajo para pruebas
+                                  // a corta distancia y evitar saturar al receptor
+  LoRa.setSyncWord(0x12);         // Palabra de sincronización privada por defecto para SX127X 
+                                  // Usaremos la palabra de sincronización para crear diferentes
+                                  // redes privadas por equipos
+  LoRa.setPreambleLength(8);      // Número de símbolos a usar como preámbulo
+
+  
+  // Indicamos el callback para cuando se reciba un paquete
+  LoRa.onReceive(onReceive);
+  
+  // Activamos el callback que nos indicará cuando ha finalizado la 
+  // transmisión de un mensaje
+  LoRa.onTxDone(TxFinished);
+
+  // Nótese que la recepción está activada a partir de este punto
+  LoRa.receive();
+
+  Serial.println("LoRa init succeeded.\n");
+}
+
+// --------------------------------------------------------------------
+// Loop function
+// --------------------------------------------------------------------
+void loop() {
+
+}
+
+// --------------------------------------------------------------------
+// Receiving message function
+// --------------------------------------------------------------------
+void onReceive(int packetSize) 
+{
+  if (transmitting && !txDoneFlag) txDoneFlag = true;
+  
+  if (packetSize == 0) return;          // Si no hay mensajes, retornamos
+
+  // Leemos los primeros bytes del mensaje
+  uint8_t buffer[10];                   // Buffer para almacenar el mensaje
+  int recipient = LoRa.read();          // Dirección del destinatario
+  uint8_t sender = LoRa.read();         // Dirección del remitente
+                                        // msg ID (High Byte first)
+  uint16_t incomingMsgId = ((uint16_t)LoRa.read() << 7) | 
+                            (uint16_t)LoRa.read();
+  
+  uint8_t incomingLength = LoRa.read(); // Longitud en bytes del mensaje
+  
+  uint8_t receivedBytes = 0;            // Leemos el mensaje byte a byte
+  while (LoRa.available() && (receivedBytes < uint8_t(sizeof(buffer)-1))) {            
+    buffer[receivedBytes++] = (char)LoRa.read();
+  }
+  
+  if (incomingLength != receivedBytes) {// Verificamos la longitud del mensaje
+    Serial.print("Receiving error: declared message length " + String(incomingLength));
+    Serial.println(" does not match length " + String(receivedBytes));
+    return;                             
+  }
+
+  // Verificamos si se trata de un mensaje en broadcast o es un mensaje
+  // dirigido específicamente a este dispositivo.
+  // Nótese que este mecanismo es complementario al uso de la misma
+  // SyncWord y solo tiene sentido si hay más de dos receptores activos
+  // compartiendo la misma palabra de sincronización
+  if ((recipient & localAddress) != localAddress ) {
+    Serial.println("Receiving error: This message is not for me.");
+    return;
+  }
+
+  // Imprimimos los detalles del mensaje recibido
+  Serial.println("Received from: 0x" + String(sender, HEX));
+  Serial.println("Sent to: 0x" + String(recipient, HEX));
+  Serial.println("Message ID: " + String(incomingMsgId));
+  Serial.println("Payload length: " + String(incomingLength));
+  printBinaryPayload(buffer, receivedBytes);
+
+  LoRa.receive();
+}
+
+void TxFinished()
+{
+  txDoneFlag = true;
+}
+
+union {
+    uint8_t bytes[4];
+    long valor; // Usa signed long para admitir valores negativos
+} conversor;
+
+void printBinaryPayload(uint8_t * payload, uint8_t payloadLength)
+{
+  // Verificar que el payload tiene al menos 8 bytes
+  if (payloadLength < 8) {
+    Serial.println("Error: Payload demasiado corto, no se pueden obtener latitud y longitud.");
+    return;
+  }
+
+  // Procesar los primeros 4 bytes para la latitud (ajustando endian)
+  conversor.bytes[0] = payload[3];
+  conversor.bytes[1] = payload[2];
+  conversor.bytes[2] = payload[1];
+  conversor.bytes[3] = payload[0];
+  long Lat = conversor.valor;
+  Serial.print("Lat: "); Serial.println(Lat);
+
+  // Procesar los siguientes 4 bytes para la longitud (ajustando endian)
+  conversor.bytes[0] = payload[7];
+  conversor.bytes[1] = payload[6];
+  conversor.bytes[2] = payload[5];
+  conversor.bytes[3] = payload[4];
+  long Long = conversor.valor;
+  Serial.print("Long: "); Serial.println(Long);
+
+  Serial.print("SIV: "); Serial.println(payload[8]);
+}
